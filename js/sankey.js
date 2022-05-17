3cba1ddb47498d56220360f5a08f0f3095a08b2a
anthonydouc <anthonydouc@hotmail.com>
2022-05-17 16:22:49 +1000

    add support for date filtering

--- js/sankey.js
+++ js/sankey.js
@@ -1,89 +1,175 @@
 google.charts.load('current', { 'packages': ['sankey'] });
 google.charts.setOnLoadCallback(drawChart);
 
+
+function trunc_values(swaps, amounts, volume_type) {
+
+    // calculate maximum value of selected entries. This is used to enforce a minimum weight on very small entries.
+    max = Math.max(...amounts);
+
+    for (var i = 0; i < swaps.length; i++) {
+
+        if (volume_type == 'Total USD volume') {
+            swaps[i]['AMOUNT'] = Math.max(max * 0.001, swaps[i].AMOUNT);
+        }
+        else if (volume_type == 'Total RUNE volume') {
+            swaps[i]['AMOUNT_RUNE'] = Math.max(max * 0.001, swaps[i].AMOUNT_RUNE);
+        }
+
+    };
+
+    return swaps
+}
+
+
+function filter_by_asset(swaps, selected_asset, direction, include_synths, volume_type) {
+    var amounts = [];
+    var swaps_flt = [];
+
+    for (var i = 0; i < swaps.length; i++) {
+
+        from = swaps[i].FROM_ASSET;
+        to = swaps[i].TO_ASSET;
+
+        from_type = swaps[i].FROM_TYPE;
+        to_type = swaps[i].TO_TYPE;
+
+        var include = false;
+        var asset_received = (from != selected_asset) && (to == selected_asset);
+        var asset_offered = (from == selected_asset) && (to != selected_asset);
+
+        var inc_synths = (include_synths == 'Assets & Synths') || (include_synths == 'Synths only')
+        var inc_assets = (include_synths == 'Assets & Synths') || (include_synths == 'Assets only')
+
+        var offered_type_inc = ((to_type == 'synth') && inc_synths) || ((to_type == 'asset') & inc_assets);
+
+        var received_type_inc = ((from_type == 'synth') && inc_synths) || (from_type == 'asset' & inc_assets);
+
+        if ((direction == "Received") && asset_received && received_type_inc) {
+            include = true
+        }
+        else if ((direction == "Offered") && asset_offered && offered_type_inc) {
+            include = true
+        }
+
+        if (include) {
+            amounts.push(parseFloat(swaps[i].AMOUNT))
+            swaps_flt.push(swaps[i])
+        }
+
+    };
+
+    swaps = trunc_values(swaps_flt, amounts, volume_type)
+    return swaps_flt
+}
+
+
+function filter_by_date(swaps, mindate, maxdate) {
+    // keep only data between min & max dates.
+    // TODO: confirm time zones are consistent between calendar and flipside.
+    swaps_flt = [];
+
+    for (var i = 0; i < swaps.length; i++) {
+        var day = new Date(swaps[i]['DAY'])
+        if ((day >= mindate) && (day <= maxdate)) {
+            swaps_flt.push(swaps[i])
+        }
+    }
+
+    return swaps_flt
+}
+
+function agg_by_asset(swaps, agg_field, val_field) {
+
+    var result = [];
+
+    swaps.reduce(function (res, value) {
+
+        if (!res[value[agg_field]]) {
+            res[value[agg_field]] = { [agg_field]: value[agg_field], [val_field]: 0 };
+            result.push(res[value[agg_field]])
+        }
+        res[value[agg_field]][val_field] += value[val_field];
+        return res;
+    }, {});
+
+    console.log(result)
+    return result
+}
+
 function drawChart() {
 
     var json = $.ajax({
-        url: "https://api.flipsidecrypto.com/api/v2/queries/18af3d33-c935-4148-9f4f-f30c28d3dcfb/data/latest",
+        url: "https://api.flipsidecrypto.com/api/v2/queries/816e3cff-997c-4151-8300-e5e0f97a39de/data/latest",
         dataType: "json",
         success: function (jsonData) {
+
             var data = new google.visualization.DataTable();
             var selected_asset = document.getElementById("selected_asset").value;
             var direction = document.getElementById("direction").value;
             var volume_type = document.getElementById("volume_type").value;
             var include_synths = document.getElementById("include_synths").value;
 
+            const calendar = document.querySelector('#daterange');
+            var mindate = new Date(calendar.bulmaCalendar.date['start']);
+            var maxdate = new Date(calendar.bulmaCalendar.date['end']);
+
             data.addColumn('string', "FROM");
             data.addColumn('string', "TO");
             data.addColumn('number', "WEIGHT");
             data.addColumn({ type: 'string', role: 'tooltip' });
 
-            var amounts = [];
-            var included = [];
+            if (volume_type == 'Total USD volume') {
+                var vol_field = 'AMOUNT';
+            }
+            else {
+                var vol_field = 'AMOUNT_RUNE';
+            }
+
+            if (direction == "Received") {
+                var agg_field = 'FROM_ASSET';
+            }
+            else {
+                var agg_field = 'TO_ASSET';
+            }
+
             var tooltips = [];
 
-            // first determine which entries are included based on selected filters.
-            for (var i = 0; i < jsonData.length; i++) {
-                from = jsonData[i].FROM_ASSET;
-                to = jsonData[i].TO_ASSET;
-                from_type = jsonData[i].FROM_TYPE;
-                to_type = jsonData[i].TO_TYPE;
+            // filter by date
+            swaps = filter_by_date(jsonData, mindate, maxdate)
 
-                var include = false;
-                var asset_received = (from != selected_asset) && (to == selected_asset);
-                var asset_offered = (from == selected_asset) && (to != selected_asset);
+            // filter by asset type and direction
+            swaps = filter_by_asset(swaps, selected_asset, direction, include_synths, volume_type)
 
-                var inc_synths = (include_synths == 'Assets & Synths') || (include_synths == 'Synths only')
-                var inc_assets = (include_synths == 'Assets & Synths') || (include_synths == 'Assets only')
+            // aggregate over days
 
-                var offered_type_inc = ((to_type == 'synth') && inc_synths) || ((to_type == 'asset') & inc_assets);
+            swaps = agg_by_asset(swaps, agg_field, vol_field)
 
-                var received_type_inc = ((from_type == 'synth') && inc_synths) || (from_type == 'asset' & inc_assets);
-
-                if ((direction == "Received") && asset_received && received_type_inc) {
-                    include = true
-                }
-                else if ((direction == "Offered") && asset_offered && offered_type_inc) {
-                    include = true
-                }
-
-                if (include) {
-                    amounts.push(parseFloat(jsonData[i].AMOUNT))
-                    included.push(true)
+            for (var i = 0; i < swaps.length; i++) {
+                if (direction == "Received") {
+                    to = selected_asset
+                    from = swaps[i].FROM_ASSET;
                 }
                 else {
-                    included.push(false)
-                };
+                    to = swaps[i].TO_ASSET;
+                    from = selected_asset
+                }
 
+                amount = swaps[i][vol_field]
+
+                if (volume_type == 'Total USD volume') {
+                    amount_currency = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumSignificantDigits: 12 }).format(amount)
+                }
+                else if (volume_type == 'Total RUNE volume') {
+                    amount_currency = Intl.NumberFormat('en-US', { maximumSignificantDigits: 12 }).format(amount)
+                }
+
+                tooltip = '<div style="margin: 5px;"><p style=" width:150px;"><strong>' + from + '</strong> to <strong>' + to + '</strong></p><span style="color:#ffffff">' + volume_type + ': ' + amount_currency + '</span></div>';
+
+                data.addRow([from, to, amount, tooltip]);
             };
 
-            // calculate maximum value of selected entries. This is used to enforce a minimum weight on very small entries.
-            max = Math.max(...amounts);
-
-            for (var i = 0; i < jsonData.length; i++) {
-                if (included[i]) {
-
-                    from = jsonData[i].FROM_ASSET;
-                    to = jsonData[i].TO_ASSET;
-                    from_type = jsonData[i].FROM_TYPE;
-                    to_type = jsonData[i].TO_TYPE;
-
-                    if (volume_type == 'Total USD volume') {
-                        amount = jsonData[i].AMOUNT;
-                        amount_currency = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumSignificantDigits: 12 }).format(amount)
-                    }
-                    else if (volume_type == 'Total RUNE volume') {
-                        amount = jsonData[i].AMOUNT_RUNE;
-                        amount_currency = Intl.NumberFormat('en-US', { maximumSignificantDigits: 12 }).format(amount)
-                    }
-
-                    tooltip = '<div style="margin: 5px;"><p style=" width:150px;"><strong>' + from + '</strong> to <strong>' + to + '</strong></p><span style="color:#ffffff">' + volume_type + ': ' + amount_currency + '</span></div>';
-                    
-                    data.addRow([from, to, Math.max(max * 0.001, amount), tooltip]);
-                };
-            };
-
-            // Sets chart options.
+            // Set chart options.
             var options = {
                 tooltip: { isHtml: true },
                 sankey: {
@@ -102,27 +188,10 @@ function drawChart() {
                 },
             };
 
-            // Instantiates and draws our chart, passing in some options.
+            // Instantiate and draw chart, passing in some options.
             var chart = new google.visualization.Sankey(document.getElementById('sankey_from'));
             chart.draw(data, options);
         }
     });
 }
 
-window.onload = (function () {
-    var json = $.ajax({
-        url: "https://api.flipsidecrypto.com/api/v2/queries/044ea8ff-9093-49d2-b440-045ea4a83597/data/latest",
-        dataType: "json",
-        success: function (jsonData) {
-            options = jsonData[0]['ASSETS'].split(',')
-
-            select = document.getElementById("selected_asset");
-            for (var i = 0; i <= options.length - 1; i++) {
-                option = document.createElement("option");
-                option.innerHTML = options[i];
-                select.appendChild(option);
-            }
-        }
-    });
-
-});
